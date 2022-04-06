e2d2eab4a4c2747a294d7d522c1ce5c7bc8eaa4b
MD Parveen <100269535+ParveenMD@users.noreply.github.com>
2022-04-06 07:48:28 +0000

    Add files via upload

---
+++ 12.Help.ps1
@@ -0,0 +1,11 @@
+﻿# 12.Write a command to search details about other cmdlet , Hint - "How many ways to use the help command with different- different parameter ?
+
+
+write-host ("MD Parveen") -ForegroundColor Green
+
+Help Select-Object 
+Help Select-Object -Full
+Help Select-Object -Detailed
+Help Select-Object -Examples
+Help Select-Object -Online 
+Help Select-Object -Parameter *
---
+++ 3.NewUser.ps1
@@ -0,0 +1,15 @@
+﻿# 3.Write a script to create a new user in active directory.
+
+write-host ("MD Parveen") -ForegroundColor Green 
+
+#Import-Module ServerManager
+#Add-WindowsFeature -Name "RSAT-AD-PowerShell" -IncludeAllSubFeature
+Import-Module ActiveDirectory
+
+$AccountName=read-host("enter the user name")
+
+New-ADUser $AccountName
+
+Get-ADUser -Filter * -Properties samAccountName | select samAccountName
+
+write-host("user added successfully") -ForegroundColor Red
