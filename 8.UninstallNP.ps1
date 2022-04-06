dc053ab7e2659af7690b65e91f17892c805939c4
MD Parveen <100269535+ParveenMD@users.noreply.github.com>
2022-04-06 10:17:25 +0000

    Add files via upload

---
+++ 8.UninstallNP.ps1
@@ -0,0 +1,15 @@
+﻿# 8. Write a script to uninstall to notepad++ in all the server
+
+
+write-host ("MD Parveen") -ForegroundColor Green 
+
+$servers = Get-Content -Path C:\md.parveen\Servers\servers.txt
+foreach ($server in $servers){
+Write-Host "Unistalling Notepad++ in $server" -ForegroundColor DarkYellow
+$app =  Get-WmiObject -class win32_Product -ComputerName $server | where { $_.name -eq "Notepad++ (64-bit)" }
+Write-Host "Notepad++ is unistalled in $server " -ForegroundColor Green
+}
+
+
+
+
