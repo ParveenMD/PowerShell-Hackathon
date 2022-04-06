714bcbb58aa0bcd96d6499de13f2853ae9f99735
MD Parveen <100269535+ParveenMD@users.noreply.github.com>
2022-04-06 10:23:26 +0000

    Add files via upload

---
+++ 13.Eventlogs.ps1
@@ -0,0 +1,11 @@
+﻿#13.Write a script to display the event logs for the following ID and Logname for the all servers.
+#ID= 4624,4672,9027
+#Logname = 'System', Security', Applcation'
+
+
+write-host ("MD Parveen") -ForegroundColor Green
+Get-EventLog -logname System
+Get-EventLog -LogName Security
+Get-EventLog -LogName Application
+Get-EventLog -LogName application | Where-Object {$_.InstanceID -eq '4624','4672','9027'}
+write-host ("output")
