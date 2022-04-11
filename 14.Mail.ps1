d1a41ac81325ac4233171e860396ef4a7292fc6a
MD Parveen <100269535+ParveenMD@users.noreply.github.com>
2022-04-11 06:09:20 +0000

    Add files via upload

---
+++ 14.Mail.ps1
@@ -0,0 +1,19 @@
+﻿# 14.Write a script for sending a mail to mohit.my844@outlook.com with the details in below snip when CPU, Disk, Memory utilization above the threshold value. Take Threshold value as 60.
+
+write-host ("MD Parveen") -ForegroundColor Green 
+
+$mymail="parveensamreen@outlook.com"
+
+$smtp = "smtp.outlook.com"
+
+$to = "mohit.my844@outlook.com"
+
+$sub="power shell task"
+
+$body= "Welcome to PowerShell Training"
+
+$creds = (Get-Credential -Credential "$myemail")
+
+start-sleep 2
+
+Send-MailMessage -To $to -From $mymail -Subject $sub -Body $body -SmtpServer $smtp -Credential $creds -UseSsl -Port 578 -DeliveryNotificationOption OnSuccess
---
+++ 7.InstallNP.ps1
@@ -0,0 +1,33 @@
+﻿# 7.Write a script to install to notepad++ in all the server
+
+
+write-host ("MD Parveen") -ForegroundColor Green 
+
+Write-Host "Starting installation..."
+
+Write-Host "installing Notepad++..."
+
+$File = PowerShell "& 'C:\Users\Administrator.Demo\Desktop\npp.7.9.3.installer.exe'"
+
+$Check = Test-Path $File
+
+If ($Check -eq $True)
+
+{
+
+PowerShell "& 'C:\Users\Administrator.Demo\Desktop\npp.7.9.3.installer.exe'"
+
+Write-Host "Notepad++ installed!"
+
+}
+
+Else
+
+{
+
+Write-Host "Notepad++ is installed."
+
+}
+
+
+#write-host "MD Parveen"Powershell.exe -ExecutionPolicy Bypass .\Deploy-Notepad++.ps1 -DeploymentType "Install" -DeployMode "Interactive"
